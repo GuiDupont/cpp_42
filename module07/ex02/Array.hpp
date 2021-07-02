79941d9f25ddb01f2b9a9e55c911aeb0617114af
gdupont <gdupont@student.42.fr>
2021-07-02 14:55:57 +0200

    wip 07

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,12 +6,15 @@
 /*   By: gdupont <gdupont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 10:46:29 by gdupont           #+#    #+#             */
-/*   Updated: 2021/07/02 12:09:16 by gdupont          ###   ########.fr       */
+/*   Updated: 2021/07/02 14:54:23 by gdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_H
 #define ARRAY_H
+
+#include <exception>
+#include <iostream>
 
 template <typename T>
 class Array {
@@ -19,30 +22,67 @@ class Array {
 	public:
 		int	size(void) const;
 
-		Array(void) : _array(new T[0]) { };
+		Array(void) : _array(new T[0]), _size(0) { };
 		Array(unsigned int n) : _array(new T[n]), _size(n) { };
 		
 		Array(Array & rhs);
+
+		~Array(void);
 		
-		Array const & operator=(Array const & rhs);
-		Array const & operator[](Array const & rhs);
+		Array<T> const & operator=(Array const & rhs);
+		T & operator[](int n);
 
 	private:
 		T 				*_array;
-		unsigned int 	_size;
+		int 	_size;
 };
+
+template<typename T>
+int	Array<T>::size(void) const {
+	return (this->_size);
+}
+
+template<typename T>
+Array<T>::Array(Array & rhs) {
+	this->_size = 0;
+	*this = rhs;
+}
 
 template<typename T>
 Array<T> const & Array<T>::operator=(Array<T> const & rhs) {
 	if (this == &rhs)
 		return (*this);
-	delete [] this->_array;
-	this->_array = new T[rhs->_size];
-	this->_size = rhs->_size;
+	if (this->_array && this->_size)
+		delete [] this->_array;
+	this->_array = new T[rhs._size];
+	this->_size = rhs._size;
 	for (int i = 0; i < this->_size; i++) {
-		this->_array[i] = rhs->_array[i];
+		this->_array[i] = rhs._array[i];
 	}
 	return (*this);
+}
+
+template<typename T>
+T & Array<T>::operator[](int n) {
+	if (n >= this->_size || n < 0)
+		throw (std::exception());
+	return (this->_array[n]);
+}
+
+template<typename T>
+Array<T>::~Array(void) {
+	delete [] this->_array;
+}
+
+template<typename T>
+void	printElem(Array<T> &array, int i) {
+	try 
+	{
+		std::cout << array[i];
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
 }
 
 #endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gdupont <gdupont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 10:46:32 by gdupont           #+#    #+#             */
-/*   Updated: 2021/07/02 11:51:01 by gdupont          ###   ########.fr       */
+/*   Updated: 2021/07/02 14:55:42 by gdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,37 @@
 
 int main(void)
 {
-	;
+	Array<int> arrayInt(5);
+	std::cout << "Avant de changer la valeur : ";
+	printElem(arrayInt, 0);
+	arrayInt[0] = 2;
+	std::cout << "\nApres avoir changé la valeur : ";
+	printElem(arrayInt, 0);
+	
+	std::cout << "\nEn essayant d'acceder a un index hors-limites : ";	
+	printElem(arrayInt, 6);
+	
+	Array<std::string> arrayString(5);
+	std::cout << "\n\nAvant de changer la valeur : ";
+	printElem(arrayString, 3);
+	arrayString[3] = "Coucou les templates c'est trop genial\n";
+	std::cout << "\nApres avoir changé la valeur : ";
+	printElem(arrayString, 3);
+
+	Array<std::string> arrayStringCopy(arrayString);
+	std::cout << "\nOn cree une copie : ";
+	std::cout << "\nIndex 3 de la copie : ";
+	printElem(arrayStringCopy, 3);
+	arrayString[3] = "Coucou\n";
+	std::cout << "\nIndex 3 de l'originale apres modification : ";
+	printElem(arrayString, 3);
+	std::cout << "\nIndex 3 de la copie --> n'a pas change : ";
+	printElem(arrayStringCopy, 3);
+
+	std::cout << "\nSize de l'originale: " << arrayString.size();
+	std::cout << "\nSize de la copie: " << arrayStringCopy.size();
+	std::cout << "\n";
+
+
+	return (0);
 }
